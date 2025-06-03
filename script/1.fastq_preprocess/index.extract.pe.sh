e72653fa20f7df50ff1ab6f997e94605e37473f8
Tang.Xiaojia@mayo.edu <m092409@mforgehn2.mayo.edu>
2025-06-03 15:18:07 -0500

    update pair-end script

--- script/1.fastq_preprocess/index.extract.pe.sh
+++ script/1.fastq_preprocess/index.extract.pe.sh
@@ -2,7 +2,6 @@
 sample_id=$1  # pure numeric
 path=/research/bsi/projects/PI/tertiary/Kannan_Nagarajan_m161624/s214639.CIC_calculator/processing/batch_Feb2023
 file=$( ls $path/FastQ/BC_Seq$sample_id/*_1.fq.gz)
-python $path/index_extact_and_count.pe.py $file  > $path/results/BC_Seq${sample_id}.temp 
-cat $path/results/BC_Seq${sample_id}.temp | grep "," > $path/results/BC_Seq${sample_id}.counts.txt
-cat $path/results/BC_Seq${sample_id}.temp | grep -v "," | gzip > $path/results/BC_Seq${sample_id}.missing.gz
-rm $path/results/BC_Seq${sample_id}.temp
+python $path/index_extact_and_count.pe.py $file 
+
+ 
